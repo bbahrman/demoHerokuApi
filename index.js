f8701c626e9bef2b725eb20f65825846b7c9e9b6
Ben Bahrman <bbahrman@eidebailly.com>
2018-05-21 22:46:23 -0600

    Improved logging

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app
+    .use(bodyParser.json())
     .get('/', onGet)
     .post('/', onPost)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 function onGet (req, res) {
-    console.log(req);
+    console.log(req.body);
     res.send('Hello world - GET')
 }
 
 function onPost(req, res) {
-    console.log(req);
+    console.log(req.body);
     res.send('Hello World - POST');
 }
