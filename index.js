8f750248c3705fc924bf6463cd6abf6441df1c28
Ben Bahrman <bbahrman@eidebailly.com>
2018-05-21 19:45:19 -0600

    Re-wrote, app is working but refactored into functions

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    console.log('Get Req = ' + req);
-    res.send('Hello world!');
-}).post('/', (req, res) => {
-    console.log('Post Req = ' + req);
-    res.send('Hello world!');
-}).listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app
+    .get('/', onGet)
+    .post('/', onPost)
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+function onGet (req, res) {
+    res.send('Hello world - GET')
+}
 
+function onPost(req, res) {
+   res.send('Hello World - POST');
+}
