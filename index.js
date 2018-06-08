ad32f09e635def9751470efdb5c90324519fd8f3
Ben Bahrman <bbahrman@eidebailly.com>
2018-06-08 16:20:53 -0700

    Refactor to add notes

--- index.js
+++ index.js
@@ -31,8 +31,21 @@ function onGet (req, res) {
   console.log('Calling request');
   request(options, function (error, response, body) {
     console.log('Response returned from remote, processing response object');
-    const finalData = processData(body);
-    res.send(finalData);
+    // return a mapping of job ids to either the parent name or the job's name (both in object)
+    const jobDictionary = makeDictionary(body);
+    // get
+    const timePerJob = summarizeTimePerJob(body, jobDictionary);
+
+    Object.keys(timePerJob['summary']).forEach(jobId => {
+      timePerJob['summary'][jobId] = Math.round((timePerJob['summary'] / (60*60)) * 100) / 100;
+    });
+
+    Object.keys(timePerJob['detail']).forEach(jobId => {
+      timePerJob['detail'][jobId]['time'] = Math.round((timePerJob['detail'][jobId]['time'] / (60*60)) * 100) / 100;
+    });
+
+
+    res.send(timePerJob);
   });
   console.log('End onGet');
 }
@@ -42,31 +55,54 @@ function onPost(req, res) {
     res.send('Hello World - POST');
 }
 
-function processData (responseBody) {
+function summarizeTimePerJob (responseBody, mapping) {
   const bodyData = JSON.parse(responseBody);
-  const timePerJob = {};
   const timesheetData = bodyData['results']['timesheets'];
-  const jobCodeData = bodyData['supplemental_data']['jobcodes'];
-  const jobCodeDictionary = {};
   const now = new Date();
+  const timePerJob = {
+    summary: {},
+    detail: {}
+  };
 
   Object.keys(timesheetData).forEach((entryId) => {
-    console.log(timesheetData[entryId]['duration']);
     let duration;
+    const entryObject = timesheetData[entryId]
+    const notes = entryObject['notes'];
+    const jobId = entryObject['jobcode_id'];
+    const parent = mapping['idToParentName'][jobId];
+    const linkName = mapping['idToLink'][jobId];
+
     if(timesheetData[entryId]['on_the_clock']) {
       duration = (now - Date.parse(timesheetData[entryId]['start'])) / 1000;
       console.log('Duration calculation, seconds = ' + duration);
     } else {
       duration = parseInt(timesheetData[entryId]['duration']);
     }
-    timePerJob[timesheetData[entryId]['jobcode_id']] = timePerJob[timesheetData[entryId]['jobcode_id']] ? timePerJob[timesheetData[entryId]['jobcode_id']] + duration : duration;
+
+    // add to parent duration to parent
+    timePerJob.summary[parent] = timePerJob.summary[parent] ? timePerJob.summary[parent] + duration : duration;
+    // fill details in
+    timePerJob.detail[linkName]['time'] = timePerJob.detail[linkName]['time'] ? timePerJob.detail[linkName]['time'] + duration : duration;
+    timePerJob.detail[linkName]['notes'] = timePerJob.detail[linkName]['notes'] ? timePerJob.detail[linkName]['notes'] + ' ' + notes : notes;
   });
 
+  return timePerJob;
+}
+
+function makeDictionary (responseBody) {
+  const bodyData = JSON.parse(responseBody);
+  const jobCodeData = bodyData['supplemental_data']['jobcodes'];
   const parentIdToName = {};
+  const jobIdToLink = {};
+  const jobCodeDictionary = {};
+
+  // iterate through jobs
   Object.keys(jobCodeData).forEach(jobId => {
     if(jobCodeData[jobId]['has_children']) {
       parentIdToName[jobId] = jobCodeData[jobId]['name'];
     }
+    const nameSplit = jobCodeData[jobId]['name'].split('-');
+    jobIdToLink[jobId] = nameSplit[nameSplit.length -1].trim();
   });
 
   Object.keys(jobCodeData).forEach(jobId => {
@@ -76,14 +112,8 @@ function processData (responseBody) {
       jobCodeDictionary[jobId] = jobCodeData[jobId]['name'];
     }
   });
-  const finalData = {};
-  Object.keys(timePerJob).forEach(jobId => {
-    finalData[jobCodeDictionary[jobId]] = finalData[jobCodeDictionary[jobId]] ? finalData[jobCodeDictionary[jobId]] + timePerJob[jobId] : timePerJob[jobId];
-  });
-
-  Object.keys(finalData).forEach(jobId => {
-    finalData[jobId] = Math.round((finalData[jobId] / (60*60)) * 100) / 100;
-  });
-
-  return finalData;
+  return {
+    idToParentName: jobCodeDictionary,
+    idToLink: jobIdToLink
+  };
 }
